697294a4d4809b156fb4ecdcc7c4a419e35c2369
Ibis <lausib@yahoo.com>
2022-12-12 13:20:45 -0800

    na wa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - priting lines of code with puts
+/**
+ * main -priting lines of code with puts
  *
  * Return: 0
 */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
